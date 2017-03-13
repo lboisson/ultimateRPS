b8650a7a09d01420d6c7a114d18343000dd6c573
leo <boisson.leo@gmail.com>
2017-03-14 00:25:29 +0100

    backend ; partie game qui fonctionne

--- game/models.py
+++ game/models.py
@@ -13,72 +13,102 @@ HANDS = (
 )
 
 class Game(models.Model):
-	PlayerOne = models.ForeignKey(User, related_name='PlayerOne', null=True)
-	PlayerTwo = models.ForeignKey(User, related_name='PlayerTwo', null=True)
-	PlayerOneHand = models.CharField(max_length=10, choices=HANDS, default=NULL)
-	PlayerTwoHand = models.CharField(max_length=10, choices=HANDS, default=NULL)
-	Date = models.DateTimeField(auto_now_add=True)
-	#Outcome = models.ForeignKey(User, related_name='Winner', blank=True, null=True)
-	
 
-#	surchage de save, pas necessaire a priori
-#
-#	def save(self):
-#		if 	not self.Outcome :
-#			if self.PlayerTwoHand is ROCK :
-#				if self.PlayerOneHand is ROCK :
-#					self.Outcome = null
-#				elif self.PlayerOneHand is PAPER :
-#					self.Outcome = self.PlayerOne
-#				elif self.PlayerOneHand is SCISSORS :
-#					self.Outcome = self.PlayerTwo
-#
-#			elif self.PlayerTwoHand is PAPER :
-#				if self.PlayerOneHand is ROCK :
-#					self.Outcome = self.PlayerTwo
-#				elif self.PlayerOneHand is PAPER :
-#					self.Outcome = null
-#				elif self.PlayerOneHand is SCISSORS :
-#					self.Outcome = self.PlayerOne
-#
-#			elif self.PlayerTwoHand is SCISSORS :
-#				if self.PlayerOneHand is ROCK :
-#					self.Outcome = self.PlayerOne
-#				elif self.PlayerOneHand is PAPER :
-#					self.Outcome = self.PlayerTwo
-#				elif self.PlayerOneHand is SCISSORS :
-#					self.Outcome = null
-#		super(Game, self).save()
+	Creator = models.ForeignKey(
+		User, related_name='creator', null=True)
+	Opponent = models.ForeignKey(
+		User, related_name='opponent', null=True, blank=True)
+	CreatorHand = models.CharField(
+		max_length=10, choices=HANDS, default=NULL)
+	OpponentHand = models.CharField(
+		max_length=10, choices=HANDS, default=NULL)
+	Winner = models.ForeignKey(
+		User, related_name='winner', null=True, blank=True)
 
-	
-	def _get_winner(self):
-		if not self.Outcome :
-			if self.PlayerTwoHand is ROCK :
-				if self.PlayerOneHand is ROCK :
-					return null
-				elif self.PlayerOneHand is PAPER :
-					return self.PlayerOne
-				elif self.PlayerOneHand is SCISSORS :
-					return self.PlayerTwo
-
-			elif self.PlayerTwoHand is PAPER :
-				if self.PlayerOneHand is ROCK :
-					return self.PlayerTwo
-				elif self.PlayerOneHand is PAPER :
-					return null
-				elif self.PlayerOneHand is SCISSORS :
-					return self.PlayerOne
-
-			elif self.PlayerTwoHand is SCISSORS :
-				if self.PlayerOneHand is ROCK :
-					return self.PlayerOne
-				elif self.PlayerOneHand is PAPER :
-					return self.PlayerTwo
-				elif self.PlayerOneHand is SCISSORS :
-					return null
-
-	Outcome = property(_get_winner)
-
+	# dates
+	completed = models.DateTimeField(
+		null=True, blank=True)
+	created = models.DateTimeField(
+		auto_now_add=True)
+	modified = models.DateTimeField(
+		auto_now=True)
 
 	def __str__(self):
 		return "partie "+str(self.id)
+
+	#renvoie les parties en cours
+	@staticmethod
+	def get_available_games():
+		return Game.objects.filter(opponent=None, completed=None)
+
+	#renvoie le nombre de parties crees par l'user en parametre
+	@staticmethod
+	def created_count(user):
+		return Game.objects.filter(creator=user).count()
+
+	#renvoie toutes les parties qui concernent l'user
+	@staticmethod
+	def get_games_for_player(user):
+		from django.db.models import Q
+		return Game.objects.filter(Q(opponent=user) | Q(creator=user))
+
+
+	#renvoie une partie selon son id
+	@staticmethod
+	def get_by_id(id):
+		try:
+			return Game.objects.get(pk=id)
+		except Game.DoesNotExist:
+			DEBUG_PROPAGATE_EXCEPTIONS = False
+		pass
+
+	#cree une nouvelle partie
+	@staticmethod
+	def create_new(user1, user2, hand):
+		#user1 : le joueur qui cree la partie
+		#user2 : le joueur adverse
+		#hand : ce que joue le joueur
+		#renvoie la partie
+
+		new_game = Game(Creator=user1, Opponent=user2, CreatorHand=hand)
+		new_game.save()
+
+		return new_game
+
+	# enregistre le vainqueur, et note la date de fin
+	def enregistre_victoire(self, winner):
+		
+		self.winner = winner
+		self.completed = datetime.now()
+		self.save()
+
+	# reponse de l'opponent 
+	@staticmethod
+	def answer(self, user, hand):
+		#user : le joueur qui repond
+		#hand : ce que repond le joueur2
+		self.OpponentHand = hand
+
+		if self.OpponentHand is ROCK :
+			if self.CreatorHand is ROCK :
+				self.outcome = null
+			elif self.CreatorHand is PAPER :
+				self.outcome = self.Creator
+			elif self.CreatorHand is SCISSORS :
+				self.outcome = self.Opponent
+		elif self.OpponentHand is PAPER :
+			if self.CreatorHand is ROCK :
+				self.outcome = self.Opponent
+			elif self.CreatorHand is PAPER :
+				self.outcome = null
+			elif self.CreatorHand is SCISSORS :
+				self.outcome = self.Creator
+		elif self.OpponentHand is SCISSORS :
+			if self.CreatorHand is ROCK :
+				self.outcome = self.Creator
+			elif self.CreatorHand is PAPER :
+				self.outcome = self.Opponent
+			elif self.CreatorHand is SCISSORS :
+				self.outcome = null
+
+		self.Game.enregistre_victoire(winner=user)
