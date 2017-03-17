3e9f1dd7853f6bda25c401089ddfc6a5a907d347
keryann <keke2612@hotmail.fr>
2017-03-17 11:19:59 +0100

    clean

--- RPSleo/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class ConnexionForm(forms.Form):
-    username = forms.CharField(label="Nom d'utilisateur", max_length=30)
-    password = forms.CharField(label="Mot de passe", widget=forms.PasswordInput)
